ffe0099ea074e5ec2996e1c981b455f234a96751
akloul amira <amira.akloul@ens.uvsq.fr>
2017-03-01 16:06:31 +0100

    TP1 Noté : matmul2 non fonctionnel

--- TPs_Note/TP1_Rendu/CODE/driver.c
+++ TPs_Note/TP1_Rendu/CODE/driver.c
@@ -19,6 +19,7 @@ typedef struct {
 
 uint64_t rdtsc ();
 void matmul0 (int n, mytype_t a[n][n], mytype_t b[n][n], mytype_t c[n][n]);
+void matmul1 (int n, mytype_t a[n][n], mytype_t b[n][n], mytype_t c[n][n]);
 
 
 static void init_array(int n, mytype_t t[n][n])
@@ -34,6 +35,16 @@ static void init_array(int n, mytype_t t[n][n])
   }
 }
 
+static void init_array2(int n, float t[n][n])
+{
+  int i, j;
+
+  for(i=0; i<n; i++){
+    for(j=0;j<n; j++){
+      t[i][j] = ((float)rand())/RAND_MAX;
+    }
+  }
+}
 
 static void print_array(int n, mytype_t t[n][n])
 {
@@ -41,6 +52,16 @@ static void print_array(int n, mytype_t t[n][n])
 	for(i=0; i<n; i++){ 
 		for(j=0; j < n; j++){
 			printf("t=%f p=%f v=%f\n", t[i][j].t, t[i][j].p, t[i][j].v);
+		}
+	}
+}
+
+static void print_array2(int n, float t[n][n], float p[n][n], float v[n][n])
+{
+	int i, j;
+	for(i=0; i<n; i++){ 
+		for(j=0; j < n; j++){
+			printf("t=%f p=%f v=%f\n", t[i][j], p[i][j], v[i][j]);
 		}
 	}
 }
@@ -83,15 +104,59 @@ int main(int argc, char const *argv[])
 	mytype_t (*c)[n] = (mytype_t (*)[n]) pc;
 
 
+	//Struct to Array
+	//allocate arrays
+	float *pa_t = malloc (n * n * sizeof *pa_t);
+	float *pa_p = malloc (n * n * sizeof *pa_p);
+	float *pa_v = malloc (n * n * sizeof *pa_v);
+
+	float *pb_t = malloc (n * n * sizeof *pb_t);
+	float *pb_p = malloc (n * n * sizeof *pb_p);
+	float *pb_v = malloc (n * n * sizeof *pb_v);
+
+	float *pc_t = malloc (n * n * sizeof *pc_t);
+	float *pc_p = malloc (n * n * sizeof *pc_p);
+	float *pc_v = malloc (n * n * sizeof *pc_v);
+
+	//define new pointers, casted as 2D arrays
+	float (*a_t)[n] = (float (*)[n]) pa_t;
+	float (*a_p)[n] = (float (*)[n]) pa_p;
+	float (*a_v)[n] = (float (*)[n]) pa_v;
+
+	float (*b_t)[n] = (float (*)[n]) pb_t;
+	float (*b_p)[n] = (float (*)[n]) pb_p;
+	float (*b_v)[n] = (float (*)[n]) pb_v;
+
+	float (*c_t)[n] = (float (*)[n]) pc_t;
+	float (*c_p)[n] = (float (*)[n]) pc_p;
+	float (*c_v)[n] = (float (*)[n]) pc_v;
+
 
 	//init arrays
 	srand(42);
 	init_array(n, a);
 	init_array(n, b);
 
+	//Copy the struc in the array
+	int j;
+	for(i=0; i < n; i++){
+		for(j=0; j < n; j++){
+			a_t[i][j] = a[i][j].t;
+			a_p[i][j] = a[i][j].p;
+			a_v[i][j] = a[i][j].v;
+
+			b_t[i][j] = b[i][j].t;
+			b_p[i][j] = b[i][j].p;
+			b_v[i][j] = b[i][j].v;
+		} 
+	}
+
+
+
 	//Warm up
 	for(i=0; i < 2; i++){
-		matmul0 (n, a, b, c);
+		// KERNEL (n, a, b, c);
+		KERNEL(n, a_t, b_t, c_t, a_p, b_p, c_p, a_v, b_v, c_v);
 	}
 
 	//Time before loop
@@ -104,7 +169,8 @@ int main(int argc, char const *argv[])
 	/*-----------------------------------------------------*/
 
 	for(i=0; i < r; i++){
-		KERNEL (n, a, b, c);
+		// KERNEL (n, a, b, c);
+		KERNEL(n, a_t, b_t, c_t, a_p, b_p, c_p, a_v, b_v, c_v);
 	}
 
 	/*-----------------------------------------------------*/
@@ -123,10 +189,11 @@ int main(int argc, char const *argv[])
 			// (double) (duree_calcul.tv_sec) + (duree_calcul.tv_usec / 1000000.0));
 
 	//print performance  
-	printf("Perf : %.2f cycles / iteration\n", (t2 - t1) / ((float) n * n * n * r));
+	printf("Perf : %.2f cycles / iteration\n", (t2 - t1) / (((float) n) * n * n * r));
 
 	//print output
 	// print_array (n , c);
+	// print_array2(n, c_t, c_p, c_v);
 
 	free(pa);
 	free(pb);
--- TPs_Note/TP1_Rendu/CODE/kernel.c
+++ TPs_Note/TP1_Rendu/CODE/kernel.c
@@ -23,3 +23,40 @@ void matmul0 (int n, mytype_t a[n][n], mytype_t b[n][n], mytype_t c[n][n]){
 		}
 	}
 }
+
+//Change the order of the loop
+void matmul1 (int n, mytype_t a[n][n], mytype_t b[n][n], mytype_t c[n][n]){
+	int i, j, k;
+	for(j = 0; j < n ; j++){
+		for(i = 0; i < n; ++i){
+			c[i][j].t = 0.0f;
+			c[i][j].p = 0.0f;
+			c[i][j].v = 0.0f;
+			for(k = 0; k < n; k++){
+				c[i][j].t += a[i][k].t * b[k][j].t;
+				c[i][j].p += a[i][k].p * b[k][j].p;
+				c[i][j].v += a[i][k].v * b[k][j].v;
+			}
+		}
+	}
+}
+
+//Structure to array
+void matmul2(int n, float a_t[n][n], float b_t[n][n], float c_t[n][n],
+                   float a_p[n][n], float b_p[n][n], float c_p[n][n],
+                   float a_v[n][n], float b_v[n][n], float c_v[n][n])
+{
+	int i, j, k;
+	for(j = 0; j < n ; j++){
+		for(i = 0; i < n; ++i){
+			c_t[i][j] = 0.0f;
+			c_p[i][j] = 0.0f;
+			c_v[i][j] = 0.0f;
+			for(k = 0; k < n; k++){
+				c_t[i][j] += a_t[i][k] * b_t[k][j];
+				c_p[i][j] += a_p[i][k] * b_p[k][j];
+				c_v[i][j] += a_v[i][k] * b_v[k][j];
+			}
+		}
+	}
+}
